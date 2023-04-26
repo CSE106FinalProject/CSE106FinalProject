66fcd5a143c059ae215761a4b907bfcbe9b8bded
Austin-Matthews <48146570+Austin-Matthews@users.noreply.github.com>
2023-04-25 17:40:29 -0700

    Create PetTalk.sql

---
+++ PetTalk.sql
@@ -0,0 +1,33 @@
+CREATE TABLE users(
+    user_id     INT(10) NOT NULL AUTO_INCREMENT,
+    user_name   VARCHAR(30) NOT NULL,
+    user_pass   VARCHAR(255) NOT NULL,
+    UNIQUE INDEX user_name_unique (user_name),
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE topics(
+    topic_id    INT(10) NOT NULL AUTO_INCREMENT,
+    topic_name  VARCHAR(255) NOT NULL,
+    UNIQUE INDEX topic_name_unique (topic_name),
+    PRIMARY KEY (topic_id)
+);
+
+CREATE TABLE posts(
+    post_id         INT(10) NOT NULL AUTO_INCREMENT,
+    post_subject    VARCHAR(255) NOT NULL,
+    post_date		DATETIME NOT NULL,
+    post_topic      INT(10) NOT NULL,
+    post_by         INT(10) NOT NULL,
+    PRIMARY KEY (post_id)
+);
+
+CREATE TABLE reply(
+    reply_id 		INT(10) NOT NULL AUTO_INCREMENT,
+    reply_content	TEXT NOT NULL,
+    reply_date 		DATETIME NOT NULL,
+    reply_post		INT(10) NOT NULL,
+    reply_by		INT(10) NOT NULL,
+    PRIMARY KEY (reply_id)
+);
+
